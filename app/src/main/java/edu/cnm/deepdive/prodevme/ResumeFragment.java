fbc04a2f6b6297a9eb3e5ca02d5a6590fbb11352
jacchevr <jacchevr@gmail.com>
2018-03-05 08:11:58 -0700

    Added new fragments.
    Added a welcome screen fragment, a resume viewer fragment, and a resume fragment.

--- app/src/main/java/edu/cnm/deepdive/prodevme/ResumeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/prodevme/ResumeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import edu.cnm.deepdive.prodevme.models.Document;
 import edu.cnm.deepdive.prodevme.models.User;
 
@@ -26,6 +27,7 @@ public class ResumeFragment extends Fragment {
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_resume, container, false);
+    ((EditText)view.findViewById(R.id.resumeInsert)).getText().toString();
     new Thread(new Runnable() {
       @Override
       public void run() {
@@ -34,7 +36,7 @@ public class ResumeFragment extends Fragment {
         Document document = new Document();
         document.setProfession("Java Programmer");
         document.setIndustry("IT");
-        document.setResume("Resume"); //-Chris
+        document.setResume(EditText)R.id.resumeInsert; //-Chris
         document.setUserId(((MainActivity) getActivity()).getDatabase().userDao().insert(user));
 //        document.setUserId(user.getId());
     ((MainActivity)getActivity()).getDatabase().documentDao().insert(document);
@@ -44,5 +46,7 @@ public class ResumeFragment extends Fragment {
 
     return view;
   }
+
+  
 
 }
